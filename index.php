276aee1bf41ed0ec314d7b191b048eb86cf60803
htcuong27 <cuong.htan27@gmail.com>
2016-12-15 18:25:02 +0700

    Shop ver 2.0
    Fix and improve layout

--- index.php
+++ index.php
@@ -1,130 +1,81 @@
- <!--Be starts at here-->
-<html>
-<head>
-  <title>My Online Shop </title>
-  <link rel="stylesheet" href="styles/style.css" media="all" />
-</head>
 
-<body>
-  <!--Main Container starts here-->
-<div class="main_wrapper">
+<?php
+session_start();
+include("functions/functions.php");
+?>
+<?php require 'shared/header.php'; ?>
 
-<!--Header starts here-->
-  <div class="header_wrapper">
-
-    <a href="index.php"><img id="logo" src="images/templatemo_logo.png" /></a>
-    <img id="banner" src="images/ad_banner.jpg" />
-  </div>
-  <!--Header ends here-->
-
-  <!--Navagation Bar starts-->
-  <div class="menubar">
-    <ul id="menu">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="all_products.php">All Products</a></li>
-      <li><a href="customer/my_account.php">My Account</a></li>
-      <li><a href="#">Sign Up</a></li>
-      <li><a href="cart.php">Shopping Cart</a></li>
-      <li><a href="#">Contact Us</a></li>
-    </ul>
-
-<div id="form">
-  <form method="get" action="results.php" enctype="multipart/form-data">
-    <input type="text" name="user_query" placeholder="Search Product"/>
-    <input type="submit" name="search" value="Search" />
-</form>
-
-</div>
-
-  </div>
-<!--Navagation Bar ends-->
-
-<!--Content wrapper starts-->
-  <div class="content_wrapper">
-
-      <div id="sidebar">
-        <div id="sidebar_title">Categories</div>
-
-          <ul id="cats">
-              <?php getCats();?>
-          </ul>
-
-          <div id="sidebar_title">Brands</div>
-
-            <ul id="cats">
-
-              <?php getBrands();?>
-
-
-            </ul>
-
-
-      </div>
-
-      <div id="content_area">
-
-        <?php cart(); ?>
-
-        <div id="shopping_cart">
-
-            <span style="float:right;font-size:18px;padding:5px;line-height:40px;">
-
-              <?php
-
-              if(isset($_SESSION['customer_email'])){
-
-                echo "<b>Welcome: </b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'> Your</b>";
-
-              }else{
-
-                echo "<b>Welcome Guest:</b>";
-
-              }
-
-               ?>
-
-            <b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items(); ?> Total Price: <?php total_price(); ?><a href="cart.php" style="color:yellow;"> Go to Cart</a>
-
-              <?php
-
-              if(!isset($_SESSION['customer_email'])){
-
-                echo "<a href='checkout.php' style='color:orange;'>Login</a>";
-
-              }else{
-
-                echo "<a href='logout.php' style='color:orange;'>Logout</a>";
-
-              }
-
-               ?>
-
-            </span>
-
+<main class="site-content">
+    <section id="slider">
+            <!-- Carousel -->
+            <div id="shop-carousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#shop-carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#shop-carousel" data-slide-to="1"></li>
+                    <li data-target="#shop-carousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <b><h1>WELCOME TO OUR CMIIW SHOP</h1></b>
+                                <p>DON'T THINK JUST LET IT FLOW</p>
+                                <button type="button" class="btn btn-default get"><b>Join with us</b></button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="item">
+                        <div class="container">
+                                <div class="carousel-caption">
+                                    <div class="col-sm-6 col-md-6">
+                                        <b><h1>WITH US</h1></b>
+                                        <p>Patience, attentiveness, knowledge, communication, time, calming, listening, caring.</p>
+                                    </div>
+                                    <div class="col-sm-6 col-md-6">
+                                        <b><h1>WITH YOU</h1></b>
+                                        <p>Easy to use, fast, convenient, interests, trading, participated in the annual event.</p>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                        <div class="item">
+                            <div class="container">
+                                <div class="carousel-caption">
+                                    <b><h1>ONE DAY ONE KNOWLEDGE</h1></b>
+                                    <p>You don't know how to dress right? Don't worry, let's us make for you.</p>
+                                    <button type="button" class="btn btn-default get"><b>Join with us</b></button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <a class="left carousel-control" href="#shop-carousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a class="right carousel-control" href="#shop-carousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </div><!-- /#shop-carousel -->
+</section>
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="left-sidebar">
+          <div class="shipping text-center"><!--shipping-->
+            <img src="images/home/shipping.jpg" alt="" />
+          </div><!--/shipping-->
+          
         </div>
-
-        <?php  $ip = getIp();?>
-
-        <div id="products_box">
-
+      </div>
+      <?php cart(); ?>
+      <div class="col-sm-9 padding-right">
+        <div class="features_items"><!--features_items-->
+          <h2 class="title text-center">Features Items</h2>
           <?php getPro(); ?>
           <?php getCatPro(); ?>
           <?php getBrandPro(); ?>
-
         </div>
-      </div>
+      </div><!--features_items-->
+    </div>
   </div>
-<!--Content wrapper ends-->
-
-
-  <div id="footer">
-
-    <h2 style="text-align:center; padding-top:30px;">&copy; 2016 by www.goshirt.com</h2>
-
-  </div>
-
-
-</div>
-  <!--Main Container ends here-->
-</body>
-</html>
+</section>
+</main>
+<?php require 'shared/footer.php'; ?>
