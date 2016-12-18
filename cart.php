227efed01e69c9b4a192653f1bed761592b575ea
htcuong27 <cuong.htan27@gmail.com>
2016-12-18 18:08:13 +0700

    Cart and details ver 2.0
    Add layout and fix to use easy

--- cart.php
+++ cart.php
@@ -1,127 +1,28 @@
-<?php
 
-session_start();
-include("functions/functions.php");
+<?php require 'shared/header.php'; ?>
 
- ?>
-
-<html>
-<head>
-  <title>My Online Shop </title>
-  <link rel="stylesheet" href="styles/style.css" media="all" />
-</head>
-
-<body>
-  <!--Main Container starts here-->
-<div class="main_wrapper">
-
-<!--Header starts here-->
-  <div class="header_wrapper">
-
-    <a href="index.php"><img id="logo" src="images/templatemo_logo.png" /></a>
-    <img id="banner" src="images/ad_banner.jpg" />
-  </div>
-  <!--Header ends here-->
-
-  <!--Navagation Bar starts-->
-  <div class="menubar">
-    <ul id="menu">
-      <li><a href="index.php">Home</a></li>
-      <li><a href="all_products.php">All Products</a></li>
-      <li><a href="customer/my_account.php">My Account</a></li>
-      <li><a href="#">Sign Up</a></li>
-      <li><a href="cart.php">Shopping Cart</a></li>
-      <li><a href="#">Contact Us</a></li>
-    </ul>
-
-<div id="form">
-  <form method="get" action="results.php" enctype="multipart/form-data">
-    <input type="text" name="user_query" placeholder="Search Product"/>
-    <input type="submit" name="search" value="Search" />
-</form>
-
-</div>
-
-  </div>
-<!--Navagation Bar ends-->
-
-<!--Content wrapper starts-->
-  <div class="content_wrapper">
-
-      <div id="sidebar">
-        <div id="sidebar_title">Categories</div>
-
-          <ul id="cats">
-              <?php getCats();?>
-          </ul>
-
-          <div id="sidebar_title">Brands</div>
-
-            <ul id="cats">
-
-              <?php getBrands();?>
+<main class="site-content">
 
 
-            </ul>
 
+<section id="cart_items">
+    <div class="container">
+      <div class="table-responsive cart_info">
+      <form action="" method="post" enctype="multipart/form-data">
+        <table class="table table-condensed">
+          <thead>
+            <tr class="cart_menu">
+              
+              <td class="remove" style="padding: 20px">Remove</td>
+              <td class="products">Item</td>
+              <td class="description"></td>
+              <td class="quantity">Quantity</td>
+              <td class="price">Price</td>
 
-      </div>
-
-      <div id="content_area">
-
-        <?php cart(); ?>
-
-        <div id="shopping_cart">
-
-            <span style="float:right;font-size:18px;padding:5px;line-height:40px;">
-
-              <?php
-
-              if(isset($_SESSION['customer_email'])){
-
-                echo "<b>Welcome: </b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'> Your</b>";
-
-              }else{
-
-                echo "<b>Welcome Guest:</b>";
-
-              }
-
-               ?>
-               <b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items(); ?> Total Price: <?php total_price(); ?><a href="index.php" style="color:yellow;"> Back To Shop</a>
-
-              <?php
-
-              if(!isset($_SESSION['customer_email'])){
-
-                echo "<a href='checkout.php' style='color:orange;'>Login</a>";
-
-              }else{
-
-                echo "<a href='logout.php' style='color:orange;'>Logout</a>";
-
-              }
-
-?>
-            </span>
-
-        </div>
-
-        <?php  $ip = getIp();?>
-
-        <div id="products_box">
-
-        <form action="" method="post" enctype="multipart/form-data">
-          <table align="center" width="700px" bgcolor="skyblue">
-
-            <tr align="center">
-              <th>Remove</th>
-              <th>Product (S)</th>
-              <th>Quantity</th>
-              <th>Total Price</th>
             </tr>
+          </thead>
 
-            <?php
+          <?php
             $total = 0;
 
             global $con;
@@ -156,11 +57,16 @@ include("functions/functions.php");
 
               ?>
 
-            <tr align="center">
-              <td><input type="checkbox" name="remove[]" value="<?php echo $pro_id; ?>"/></td>
-              <td><?php echo $product_title; ?><br />
-              <img src="admin_area/product_images/<?php echo $product_image;?>" width="60" height="60"></td>
-              <td><input type="text" size="3" name="qty" value="<?php echo $_SESSION['qty']; ?>"/></td>
+
+          <tbody>
+
+
+
+            <tr>
+              <td class="cart_delete"><input type="checkbox"  style="margin: 19px 17px 0"    name="remove[]" value="<?php echo $pro_id; ?>"/></td>
+              <td class="cart_products"><img src="admin_area/product_images/<?php echo $product_image;?>" width="110" height="110"></td>
+              <td class="cart_description"><?php echo $product_title; ?></td>
+              <td class="cart_quantity"><input type="text" size="3" name="qty" value="<?php echo $_SESSION['qty']; ?>"/></td>
 
               <?php
                 if(isset($_POST['update_cart'])){
@@ -179,7 +85,7 @@ include("functions/functions.php");
                ?>
 
 
-              <td><?php echo "$" . $single_price; ?></td>
+              <td class="cart_price"><p><?php echo "$" . $single_price; ?></p></td>
             </tr>
 
             <?php
@@ -189,17 +95,20 @@ include("functions/functions.php");
              ?>
 
              <tr align="right">
-               <td colspan="4"><b>Sub Total: </b></td>
+               <td colspan="4"><b style="color: orange">Total: </b></td>
                <td><?php echo "$" . $total;?></td>
              </tr>
 
-             <tr align="center">
-               <td colspan="2"><input type="submit" name="update_cart" value="Update Cart" /></td>
-               <td><input type="submit" name="continue" value="Continue Shopping" /></td>
-               <td><button><a href="checkout.php" style="text-decoration:none;color:black;">Checkout</a></button></td>
-             </tr>
+             
 
           </table>
+          <div class="col-md-8">
+          </div>
+          <div class="col-md-4">
+               <a><input type="submit" name="update_cart" value="Update Cart" /></a>
+               <a><input type="submit" name="continue" value="Continue Shopping" /></a>
+               <a><button><a href="checkout.php" style="text-decoration:none;color:black;">Checkout</a></button></a>
+             </div>
 
         </form>
 
@@ -235,20 +144,10 @@ include("functions/functions.php");
 
          ?>
 
-        </div>
-      </div>
-  </div>
-<!--Content wrapper ends-->
+        </tbody>
 
+  </section> <!--/#cart_items-->
 
-  <div id="footer">
+</main>
 
-    <h2 style="text-align:center; padding-top:30px;">&copy; 2016 by www.goshirt.com</h2>
-
-  </div>
-
-
-</div>
-  <!--Main Container ends here-->
-</body>
-</html>
+  <?php require 'shared/footer.php'; ?>
--- details.php
+++ details.php
@@ -1,27 +1,25 @@
 <?php require 'shared/header.php'; ?>
-
 <main class="site-content">
-<div class="container">
+  <div class="container">
+    <?php
+    if(isset($_GET['pro_id'])){
 
-  <?php
-          if(isset($_GET['pro_id'])){
+      $product_id = $_GET['pro_id'];
 
-          $product_id = $_GET['pro_id'];
+      $get_pro = "select *from products where product_id = '$product_id'";
 
-          $get_pro = "select *from products where product_id = '$product_id'";
+      $run_pro = mysqli_query($con, $get_pro);
 
-          $run_pro = mysqli_query($con, $get_pro);
+      while($row_pro = mysqli_fetch_array($run_pro)){
 
-          while($row_pro = mysqli_fetch_array($run_pro)){
+        $pro_id = $row_pro['product_id'];
+        $pro_title = $row_pro['product_title'];
+        $pro_price = $row_pro['product_price'];
+        $pro_image = $row_pro['product_image'];
+        $pro_desc = $row_pro['product_desc'];
 
-            $pro_id = $row_pro['product_id'];
-            $pro_title = $row_pro['product_title'];
-            $pro_price = $row_pro['product_price'];
-            $pro_image = $row_pro['product_image'];
-            $pro_desc = $row_pro['product_desc'];
-
-            echo "
-                  <div class='col-sm-9'>
+        echo "
+        <div class='col-sm-9'>
           <div class='product-details'><!--product-details-->
             <div class='col-sm-5'>
               <div class='view-product'>
@@ -40,23 +38,17 @@
                       <i class='fa fa-shopping-cart'></i>Add to cart
                     </button>
                   </a>
-            
                 </span>
                 <p><b>Describe:</b></p>
                 <p>$pro_desc</p>
               </div><!--/product-information-->
             </div>
           </div><!--/product-details-->
-
-</div>
-            ";
-
-          }
-
-        }
-
-?>
-
-</div>
+        </div>
+        ";
+      }
+    }
+    ?>
+  </div>
 </main>
 <?php require 'shared/footer.php'; ?>
